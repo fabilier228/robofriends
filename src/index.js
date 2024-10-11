ec4b7e0f73bf88d1775f29b2ce23b8e409709fc4
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2024-10-11 17:17:05 +0200

    deleted create-react-app files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import { robots } from './robots'; // Make sure this file exists and contains an array of robots
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      {robots.map((robot, i) => (
+        <Card
+          key={robots[i].id} // unique key for each item (good practice in React)
+          id={robots[i].id}
+          name={robots[i].name}
+          email={robots[i].email}
+        />
+      ))}
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
