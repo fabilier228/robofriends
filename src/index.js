8aa224a45edacfe1bf59363af28b8b569688a06b
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2024-10-12 10:06:23 +0200

    fundamental changes to the app

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { robots } from './robots'; // Make sure this file exists and contains an array of robots
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      {robots.map((robot, i) => (
-        <Card
-          key={robots[i].id} // unique key for each item (good practice in React)
-          id={robots[i].id}
-          name={robots[i].name}
-          email={robots[i].email}
-        />
-      ))}
-    </div>
+    <App/>
   </React.StrictMode>
 );
 
